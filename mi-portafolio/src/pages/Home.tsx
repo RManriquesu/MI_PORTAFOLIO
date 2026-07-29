50a96b42bf7531fd8ba34367e398da7a175c76f4
RManriquesu <rmanriquesu@unsa.edu.pe>
2026-07-28 23:12:29 -0500

    Primer avance del diseño

--- mi-portafolio/src/pages/Home.tsx
+++ mi-portafolio/src/pages/Home.tsx
@@ -5,7 +5,8 @@ export default function Home() {
   return (
     <section className="hero">
       <h1>Hola, soy Ronald Manrique</h1>
-      <p>Desarrollador de software</p>
+      <p>Estudiante de Ingenieria de Sistemas</p>
+      <h2>Desarrollador de Sotware Jr</h2>
       <div className="hero-cta">
         <Link to="/proyectos" className="primary">Ver proyectos</Link>
         <Link to="/contacto" className="secondary">Contáctame</Link>
